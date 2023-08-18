d70d28de488c5f8afa5b9aafa2f5627569e77a05
Anuanushree <anuabi2408@gmail.com>
2023-08-18 23:23:39 +0530

    project done

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 // app.use("/assets", express.static(path.join("public/assets")))
-// app.use(express.static('public/assets'));
+app.use(express.static('public/assets'));
 // app.use('/image', express.static('public/assets/'));
 
 
