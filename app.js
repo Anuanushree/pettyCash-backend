b42ccb48c943070b46acdb2b42a6f0d0f2a6d71b
Anuanushree <anuabi2408@gmail.com>
2023-08-19 10:51:59 +0530

    correction in project

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const path = require('path')
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use("/assets", express.static(path.join("public/assets")))
-app.use(express.static('public/assets'));
+// app.use(express.static('public/assets'));
 // app.use('/image', express.static('public/assets/'));
 
 
